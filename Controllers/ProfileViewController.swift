4f4a5340826017d282b5354240828e58b020fbcd
adin <adin@xfields.net>
2018-03-07 18:11:54 -0500

    adding profileviewcontroller to project

---
+++ Controllers/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  MyFive
+//
+//  Created by Adin Greenfield on 3/7/18.
+//  Copyright © 2018 Adin Greenfield. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
