af49f655bb5de63a303f595346bea139847d25a8
adin <adin@xfields.net>
2018-03-07 23:14:33 -0500

    adding class

---
+++ Controllers/ModelController.swift
@@ -0,0 +1,15 @@
+//
+//  ModelController.swift
+//  MyFive
+//
+//  Created by Adin Greenfield on 3/7/18.
+//  Copyright © 2018 Adin Greenfield. All rights reserved.
+//
+
+import Foundation
+
+class ModelController {
+    
+    var chartName = " "
+    
+}
